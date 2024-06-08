4dd06269bb122e886ccee67d91bc8cf3f6588550
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-08 19:35:32 +0530

    third commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 const Layout = ({category}) => {
   return (
     <div>
-        <nav>{
+        <nav> <Link to="/">Home</Link>{
+           
         category.map((category)=>{
-          return  <Link key={category.id} to={`/category/${category.id}`} >
+          return <div> <Link key={category.id} to={`/category/${category.id}`} >
                 {category.name}
-            </Link>
+            </Link><br></br></div>
         })
         
         
