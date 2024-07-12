94e3ac2cb8ddb1609503f8d6150ab608aee6cfb3
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-12 20:48:01 +0530

    p

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,8 @@
-
-import React from "react";
-import './../styles/App.css';
-import { BrowserRouter as Router, Routes, Route, Link, useParams } from "react-router-dom"
-import Layout from "./Layout";
-import CategoryPage from "./CategoryPage";
+import React from 'react'
 
 const App = () => {
-  const category=[{id:"1",name:"PrJi"},{id:"2",name:"Women"}]
-  const allItem=[{id:"1",categoryItem:["Dhoti","Kurta","Tilak","BeadBag"]},{id:"2",categoryItem:["Sari","BeadBag","Tilak","Prasadam"]}]
   return (
-    <div>
-       <Router>
-        <Layout category={category} allItem={allItem} />
-        <Routes>
-          
-         
-          <Route path='/category/:id/*' element={ <CategoryPage allItem={allItem} /> } />
-        </Routes>
-       </Router>
-    </div>
+    <div>App</div>
   )
 }
 
